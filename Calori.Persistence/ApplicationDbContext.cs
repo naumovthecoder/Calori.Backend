284cdfba0b9edaed1deaddb6030fd15c8e9a88d6
naumovevgeny <evgeny.naumov@xpress.com.ph>
2023-10-30 23:45:38 +0600

    Added migrations and updated Dbs

--- Calori.Persistence/ApplicationDbContext.cs
+++ Calori.Persistence/ApplicationDbContext.cs
@@ -1,20 +1,20 @@
-using Calori.Domain.Models.Auth;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Calori.Persistence
-{
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            :base(options)
-        {
-            
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
-    }
-}
+// using Calori.Domain.Models.Auth;
+// using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore;
+//
+// namespace Calori.Persistence
+// {
+//     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+//     {
+//         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//             :base(options)
+//         {
+//             
+//         }
+//
+//         protected override void OnModelCreating(ModelBuilder builder)
+//         {
+//             base.OnModelCreating(builder);
+//         }
+//     }
+// }
---
+++ Calori.WebApi/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Calori.Domain.Models.Auth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calori.WebApi
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
