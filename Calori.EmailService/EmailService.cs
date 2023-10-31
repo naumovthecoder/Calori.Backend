63792f754f1553d6e4baf4036d13ab38070615e3
naumovevgeny <evgeny.naumov@xpress.com.ph>
2023-10-31 18:29:15 +0600

    Added RabbitMQ Email Queues

--- Calori.EmailService/EmailService.cs
+++ Calori.EmailService/EmailService.cs
@@ -1,68 +1,68 @@
-﻿using System;
-using System.Net;
-using System.Net.Mail;
-using System.Threading.Tasks;
-using Calori.Application.Interfaces;
-using Microsoft.Extensions.Configuration;
-
-namespace Calori.EmailService
-{
-    public class EmailService : IEmailService
-    {
-        private readonly string smtpServer;
-        private readonly int smtpPort;
-        private readonly string smtpUsername;
-        private readonly string smtpPassword;
-        private readonly IConfiguration _configuration;
-
-        public EmailService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            
-            smtpServer = _configuration["EmailSettings:SmtpServer"];
-            smtpPort = int.Parse(_configuration["EmailSettings:SmtpPort"]);
-            smtpUsername = _configuration["EmailSettings:SmtpUsername"];
-            smtpPassword = _configuration["EmailSettings:SmtpPassword"];
-        }
-
-        public async Task SendEmailAsync(string toEmail, string subject, string body)
-        {
-            bool enableSSL = true;
-
-            SmtpClient client = new SmtpClient(smtpServer)
-            {
-                Port = smtpPort,
-                Credentials = new NetworkCredential(smtpUsername, smtpPassword),
-                EnableSsl = enableSSL,
-            };
-
-            MailMessage message = new MailMessage
-            {
-                From = new MailAddress(smtpUsername),
-                Subject = subject,
-                Body = body,
-            };
-
-            message.To.Add(toEmail);
-
-            try
-            {
-                client.Send(message);
-                Console.WriteLine("Письмо успешно отправлено.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ошибка отправки письма: " + ex.Message);
-            }
-        }
-    }
-
-    public class EmailResponseException : Exception
-    {
-        private readonly string _message;
-        public EmailResponseException(string message)
-        {
-            _message = message;
-        }
-    }
-}
+﻿// using System;
+// using System.Net;
+// using System.Net.Mail;
+// using System.Threading.Tasks;
+// using Calori.Application.Interfaces;
+// using Microsoft.Extensions.Configuration;
+//
+// namespace Calori.EmailService
+// {
+//     public class EmailService : IEmailService
+//     {
+//         private readonly string smtpServer;
+//         private readonly int smtpPort;
+//         private readonly string smtpUsername;
+//         private readonly string smtpPassword;
+//         private readonly IConfiguration _configuration;
+//
+//         public EmailService(IConfiguration configuration)
+//         {
+//             _configuration = configuration;
+//             
+//             smtpServer = _configuration["EmailSettings:SmtpServer"];
+//             smtpPort = int.Parse(_configuration["EmailSettings:SmtpPort"]);
+//             smtpUsername = _configuration["EmailSettings:SmtpUsername"];
+//             smtpPassword = _configuration["EmailSettings:SmtpPassword"];
+//         }
+//
+//         public async Task SendEmailAsync(string toEmail, string subject, string body)
+//         {
+//             bool enableSSL = true;
+//
+//             SmtpClient client = new SmtpClient(smtpServer)
+//             {
+//                 Port = smtpPort,
+//                 Credentials = new NetworkCredential(smtpUsername, smtpPassword),
+//                 EnableSsl = enableSSL,
+//             };
+//
+//             MailMessage message = new MailMessage
+//             {
+//                 From = new MailAddress(smtpUsername),
+//                 Subject = subject,
+//                 Body = body,
+//             };
+//
+//             message.To.Add(toEmail);
+//
+//             try
+//             {
+//                 await client.SendMailAsync(message);
+//                 Console.WriteLine("Письмо успешно отправлено.");
+//             }
+//             catch (Exception ex)
+//             {
+//                 Console.WriteLine("Ошибка отправки письма: " + ex.Message);
+//             }
+//         }
+//     }
+//
+//     public class EmailResponseException : Exception
+//     {
+//         private readonly string _message;
+//         public EmailResponseException(string message)
+//         {
+//             _message = message;
+//         }
+//     }
+// }
---
+++ Calori.EmailService/RabbitMQEmailService.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+using Calori.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Calori.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            try
+            {
+                var emailContent = new EmailContent
+                {
+                    ToEmail = toEmail,
+                    Subject = subject,
+                    Body = body
+                };
+
+                var factory = new ConnectionFactory
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672
+                };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "email_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    var jsonContent = JsonSerializer.Serialize(emailContent);
+                    var bodyBytes = Encoding.UTF8.GetBytes(jsonContent);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true; // Гарантировать сохранение сообщений на диск
+
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "email_queue",
+                                        basicProperties: properties,
+                                        body: bodyBytes);
+
+                    Console.WriteLine("The email has been successfully added to the queue to be sent.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error when adding an email to the queue: " + ex.Message);
+            }
+        }
+    }
+
+    public class EmailContent
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
